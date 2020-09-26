2d7598798482b0bbcc9815baadb589d2f5255c19
cafetamine <arek.bystrowicz@gmail.com>
2020-09-26 13:32:01 +0200

    fix access modifiers

--- src/app/client/airly/AirlyClientConfig.ts
+++ src/app/client/airly/AirlyClientConfig.ts
@@ -6,24 +6,24 @@ export class AirlyClientConfig {
   private constructor(public readonly apiUrl: string,
                       public readonly authHeaders: HttpHeaders) { }
 
+  public static Create(isSecure: boolean, hostAddress: string, apiVersion: string, apiKey: string): AirlyClientConfig {
+    return new AirlyClientConfig(AirlyClientConfig.PrepareApiUrl(isSecure, hostAddress, apiVersion),
+                                 AirlyClientConfig.PrepareAuthHeaders(apiKey));
+  }
+
+  public static CreateFromEnvironment(): AirlyClientConfig {
+    return AirlyClientConfig.Create(environment.client.airly.isSecure,
+                                    environment.client.airly.hostAddress,
+                                    environment.client.airly.apiVersion,
+                                    environment.client.airly.apiKey);
+  }
+
   private static PrepareApiUrl(isSecure: boolean, hostAddress: string, apiVersion: string): string {
     return `${isSecure ? 'https' : 'http'}://${hostAddress}/${apiVersion}`;
   }
 
   private static PrepareAuthHeaders(apiKey: string): HttpHeaders {
     return new HttpHeaders({apikey: apiKey});
-  }
-
-  private static Create(isSecure: boolean, hostAddress: string, apiVersion: string, apiKey: string): AirlyClientConfig {
-    return new AirlyClientConfig(AirlyClientConfig.PrepareApiUrl(isSecure, hostAddress, apiVersion),
-                                 AirlyClientConfig.PrepareAuthHeaders(apiKey));
-  }
-
-  private static CreateFromEnvironment(): AirlyClientConfig {
-    return AirlyClientConfig.Create(environment.client.airly.isSecure,
-                                    environment.client.airly.hostAddress,
-                                    environment.client.airly.apiVersion,
-                                    environment.client.airly.apiKey);
   }
 
 }
