53ee7d3122f8baeaa268d9934a54663393f8551d
cafetamine <arek.bystrowicz@gmail.com>
2020-09-26 13:05:33 +0200

    defined InstallationClient

---
+++ src/app/core/application/installation/InstallationClient.ts
@@ -0,0 +1,18 @@
+import {Observable} from 'rxjs';
+import {Installation} from '../../domain/installation/Installation';
+
+export interface InstallationClient {
+
+  /**
+   * Returns list of installations near specified coordinates and fulfilling given query params.
+   *
+   * @param latitude  - of searched area
+   * @param longitude - of searched area
+   * @param distance  - radius of searched area around coordinates in KM
+   * @param limit     - maximum installations returned
+   *
+   * @return installations - observable list of installations
+   */
+  getNearestInstallations(latitude: number, longitude: number, distance: number, limit: number): Observable<Installation>;
+
+}
