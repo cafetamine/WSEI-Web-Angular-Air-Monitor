a0bf220d0e11e2b8c7cc55185ec104c5a8329a6f
cafetamine <arek.bystrowicz@gmail.com>
2020-09-27 11:40:08 +0200

    implemnt coordinates service

---
+++ src/app/service/coordinates/Coordinates.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {CoordinatesFacade} from '../../core/application/coordinates/CoordinatesFacade';
+import {Observable} from 'rxjs';
+import {GetCurrentCoordinatesResult} from '../../core/application/coordinates/result/GetCurrentCoordinatesResult';
+import {CoordinatesError} from '../../core/application/coordinates/CoordinatesError';
+
+@Injectable({providedIn: 'root'})
+export class CoordinatesService implements CoordinatesFacade {
+
+  public constructor() { }
+
+  public getCurrentCoordinates(): Observable<GetCurrentCoordinatesResult> {
+    return new Observable(subscriber => {
+      if (this.isGeolocationAvailable()) {
+        window.navigator.geolocation.getCurrentPosition(
+          position => subscriber.next(GetCurrentCoordinatesResult.Success(position)),
+          error => subscriber.next(GetCurrentCoordinatesResult.Failure(error)));
+      } else {
+        subscriber.next(GetCurrentCoordinatesResult.FailureInternal(CoordinatesError.GeolocationUnavailable));
+      }
+    });
+  }
+
+  private isGeolocationAvailable(): boolean {
+    return !! window && !!window.navigator && !!window.navigator.geolocation;
+  }
+
+}
