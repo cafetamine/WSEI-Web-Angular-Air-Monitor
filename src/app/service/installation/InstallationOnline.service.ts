6adc71846ddc0c7336c5db288dc1e94c4fc5c03d
cafetamine <arek.bystrowicz@gmail.com>
2020-09-27 12:53:01 +0200

    implemnt usage of geolocation, add error handing

--- src/app/service/installation/InstallationOnline.service.ts
+++ src/app/service/installation/InstallationOnline.service.ts
@@ -1,23 +1,46 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {AirlyClientService} from '../../client/airly/AirlyClient.service';
 import {InstallationFacade} from '../../core/application/installation/InstallationFacade';
-import {GetInstallationByLocationCommand} from '../../core/application/installation/command/GetInstallationByLocationCommand';
-import {Observable} from 'rxjs';
-import {Installation} from '../../core/domain/installation/Installation';
+import {GetInstallationsByLocationCommand} from '../../core/application/installation/command/GetInstallationsByLocationCommand';
+import {Observable, of} from 'rxjs';
+import {CoordinatesService} from '../coordinates/Coordinates.service';
+import {flatMap, map} from 'rxjs/internal/operators';
+import {GetInstallationsResult} from '../../core/application/installation/result/GetInstallationsResult';
+import {InstallationError} from '../../core/application/installation/InstallationError';
+import {GetInstallationsNearbyCommand} from '../../core/application/installation/command/GetInstallationsNearbyCommand';
 
 @Injectable({providedIn: 'root'})
 export class InstallationOnlineService implements InstallationFacade {
 
   // TODO inject by interface
-  public constructor(private readonly _client: AirlyClientService) { }
+  public constructor(private readonly _coordinatesService: CoordinatesService,
+                     private readonly _client: AirlyClientService) { }
 
-  public getInstallationsByLocation(command: GetInstallationByLocationCommand): Observable<Installation[]> {
+   public getNearbyInstallations(command: GetInstallationsNearbyCommand): Observable<GetInstallationsResult> {
+     if (!command.isValid()) {
+       return of(GetInstallationsResult.FailureInternal(InstallationError.InvalidCommandInput));
+     }
+     return this._coordinatesService.getCurrentCoordinates()
+                .pipe(flatMap(coordinatesReadResult => {
+                  if (coordinatesReadResult.isSuccess()) {
+                    const coordinates = coordinatesReadResult.getCoordinates();
+                    return this.getInstallationsByLocation(command.withCoordinates(coordinates));
+                  } else {
+                    // TODO use coordinates error instead
+                   return of(GetInstallationsResult.FailureInternal(InstallationError.GeolocationRead));
+                  }
+                }));
+   }
+
+  public getInstallationsByLocation(command: GetInstallationsByLocationCommand): Observable<GetInstallationsResult> {
     if (!command.isValid()) {
-      // TODO validate command - redirect, error mapping
-      console.error(`invalid command: ${command}`);
+      return of(GetInstallationsResult.FailureInternal(InstallationError.InvalidCommandInput));
     }
+    // TODO failures
     return this._client.fetchNearestInstallations(command.latitude, command.longitude, command.radius, command.limit)
-               .pipe(
+               .pipe(map(httpResult => {
+                   return GetInstallationsResult.Success(httpResult);
+                 })
                  // TODO handle api errors - there or in client?
                  // TODO check for null or empty responses - mby redirects?
                  // TODO DTO
